ec46d739def5ccc60316757a2c78055fdfd8066f
Vinay Rao <vinayrao7039@gmail.com>
2021-02-26 00:44:13 +0530

    Login Activity

--- app/src/main/java/com/example/quizapp/MainActivity.java
+++ app/src/main/java/com/example/quizapp/MainActivity.java
@@ -1,15 +1,82 @@
 package com.example.quizapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextInputLayout email, password ;
+    Button login;
+    
+
+    FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        email = (TextInputLayout)findViewById(R.id.email);
+        password = (TextInputLayout)findViewById(R.id.password);
+        login = findViewById(R.id.loginBtn);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String text_email = email.getEditText().getText().toString();
+                String text_password = password.getEditText().getText().toString();
+
+                if(TextUtils.isEmpty(text_email) || TextUtils.isEmpty(text_password) ){
+                    Toast.makeText(MainActivity.this, "Empty Credentials", Toast.LENGTH_SHORT).show();
+                }else{
+                    loginUser(text_email, text_password);
+                }
+            }
+        });
     }
+
+    private void loginUser(String text_email, String text_password) {
+        mAuth.signInWithEmailAndPassword(text_email, text_password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    Toast.makeText(MainActivity.this, "Update the profile " +
+                            "for better experince", Toast.LENGTH_SHORT).show();
+//                    Intent intent = new Intent(MainActivity.this , MainActivity.class);
+//                    intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP);
+//                    startActivity(intent);
+//                    finish();
+                }else{
+                    Toast.makeText(MainActivity.this, task.getException().getLocalizedMessage(), Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+//    @Override
+//    protected void onStart() {
+//        super.onStart();
+//
+//        if(mAuth.getCurrentUser() != null ){
+//            startActivity(new Intent(MainActivity.this, MainActivity.class));
+//            finish();
+//        }
+//    }
 }
